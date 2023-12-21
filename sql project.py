fbab2bb3a1fe4f1b8f860f10e70f57fabcbc0159
linyanbruce <152062080+linyanbruce@users.noreply.github.com>
2023-12-21 13:14:22 +0800

    Update sql project.py

--- sql project.py
+++ sql project.py
@@ -1,7 +1,7 @@
 import mysql.connector
 import requests
 
-# 连接到数据库
+# 连接到mysql本地的数据库 需要提前先建好'company_management'这个数据库并存入info的表，完成本地的导入
 conn = mysql.connector.connect(
     host='localhost',
     user='root',
@@ -14,7 +14,7 @@ cursor = conn.cursor()
 cursor.execute("SELECT id, address FROM address_info ")
 addresses = cursor.fetchall()
 
-# 使用高德 API 查询地址的地理信息
+# 遍历从数据库中检索到的地址数据
 for address in addresses:
     address_id = address[0]
     address_text = address[1]
@@ -24,7 +24,7 @@ for address in addresses:
     data = response.json()
     if data['status'] == '1' and int(data['count']) > 0: # 确保从 API 获取到有效的地理信息
         location = data['geocodes'][0]['location'] # 从 API 返回的数据中提取 geocodes 列表的第一个元素的 location 字段
-        latitude, longitude = location.split(',')
+        latitude, longitude = location.split(',') # 将经度与纬度分别存储
         # 将地理信息更新到数据库表格中
         cursor.execute("UPDATE address_info SET latitude = %s, longitude = %s WHERE id = %s",
                        (latitude, longitude, address_id))
@@ -34,4 +34,4 @@ for address in addresses:
 cursor.close()
 conn.close()
 
-# 先查询包含地址和 ID 的数据，然后使用高德 API 查询每个地址的经纬度信息，并将其更新到数据库表格中，测试了193行代码是有效的（丐版api），好像是请求量受到限制
+# 先查询包含地址和 ID 的数据，然后使用高德 API 查询每个地址的经纬度信息，并将其更新到数据库表格中，测试了239行代码是有效的（丐版api），好像是请求量受到限制
